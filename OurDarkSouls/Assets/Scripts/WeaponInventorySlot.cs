5d66f1089c06a7c7538e9331ef9d27947dad1072
Kamil Shernyaev <Abroham97@gmail.com>
2022-04-15 03:06:04 +0500

    EP. 27 Switch Weapons From Inventory

--- OurDarkSouls/Assets/Scripts/WeaponInventorySlot.cs
+++ OurDarkSouls/Assets/Scripts/WeaponInventorySlot.cs
@@ -7,8 +7,18 @@ namespace SG
 {
     public class WeaponInventorySlot : MonoBehaviour
     {
+        PlayerInventory playerInventory;
+        WeaponSlotManager weaponSlotManager;
+        UIManager uIManager;
         public Image icon;
         WeaponItem item;
+
+        public void Awake()
+        {
+            playerInventory = FindObjectOfType<PlayerInventory>();
+            weaponSlotManager = FindObjectOfType<WeaponSlotManager>();
+            uIManager = FindObjectOfType<UIManager>();
+        }
 
         public void AddItem(WeaponItem newItem)
         {
@@ -27,6 +37,47 @@ namespace SG
             gameObject.SetActive(false);
             icon.gameObject.SetActive(false);
 
+        }
+
+        public void EquipThisItem()
+        {
+            if(uIManager.rightHandSlot01Selected)
+            {
+                playerInventory.weaponsInventory.Add(playerInventory.weaponsInRightHandSlots[0]);
+                playerInventory.weaponsInRightHandSlots[0] = item;
+                playerInventory.weaponsInventory.Remove(item);
+            }
+            else if(uIManager.rightHandSlot02Selected)
+            {
+                playerInventory.weaponsInventory.Add(playerInventory.weaponsInRightHandSlots[1]);
+                playerInventory.weaponsInRightHandSlots[1] = item;
+                playerInventory.weaponsInventory.Remove(item);
+            }
+            else if(uIManager.leftHandSlot01Selected)
+            {
+                playerInventory.weaponsInventory.Add(playerInventory.weaponsInLeftHandSlots[0]);
+                playerInventory.weaponsInLeftHandSlots[0] = item;
+                playerInventory.weaponsInventory.Remove(item);
+            }
+            else if(uIManager.leftHandSlot02Selected)
+            {
+                playerInventory.weaponsInventory.Add(playerInventory.weaponsInLeftHandSlots[1]);
+                playerInventory.weaponsInLeftHandSlots[1] = item;
+                playerInventory.weaponsInventory.Remove(item);
+            }
+            else
+            {
+                return;
+            }
+
+            playerInventory.rightWeapon = playerInventory.weaponsInRightHandSlots[playerInventory.currentRightWeaponIndex];
+            playerInventory.leftWeapon = playerInventory.weaponsInLeftHandSlots[playerInventory.currentLeftWeaponIndex];
+
+            weaponSlotManager.LoadWeaponOnSlot(playerInventory.rightWeapon,false);
+            weaponSlotManager.LoadWeaponOnSlot(playerInventory.leftWeapon,true);  
+
+            uIManager.equipmentWindowUI.LoadWeaponOnEquipmentScreen(playerInventory);       
+            uIManager.ResetAllSelectedSlot();
         }
     }
 }
