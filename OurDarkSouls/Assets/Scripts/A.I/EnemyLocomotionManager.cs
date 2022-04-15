482aa368366bff7909bac43f01ceb599c6b72fcc
Grouli123 <69174156+Grouli123@users.noreply.github.com>
2022-04-16 02:16:44 +0300

    EP. 32 ENEMY A.I - ATTACKS
    Ударяет один раз и выдаёт ошибку. Бегает на месте

--- OurDarkSouls/Assets/Scripts/A.I/EnemyLocomotionManager.cs
+++ OurDarkSouls/Assets/Scripts/A.I/EnemyLocomotionManager.cs
@@ -57,6 +57,9 @@ namespace SG
 
         public void HandleMoveToTarget()
         {
+            if (enemyManager.isPreformingAction)
+                return;
+
             Vector3 targetDirection = currentTarget.transform.position - transform.position;
             distanceFromTarget = Vector3.Distance(currentTarget.transform.position, transform.position);
             float viewableAngle = Vector3.Angle(targetDirection, transform.position);
--- OurDarkSouls/Assets/Scripts/A.I/EnemyManager.cs
+++ OurDarkSouls/Assets/Scripts/A.I/EnemyManager.cs
@@ -1,4 +1,3 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,20 +7,27 @@ namespace SG
     public class EnemyManager : CharacterManager
     {
         EnemyLocomotionManager enemyLocomotionManager;
+        EnemyAnimatorManager enemyAnimatorManager;
         public bool isPreformingAction;
+
+        public EnemyAttackAction[] enemyAttacks;
+        public EnemyAttackAction currentAttack;
 
         [Header ("A.I. Settings")]
         public float detectionRadius = 20;        
         public float maximumDetectionAngle = 50;
         public float mininumDetectionAngle = -50;
+
+        public float currentRecoveryTime = 0;
         private void Awake() 
         {
             enemyLocomotionManager = GetComponent<EnemyLocomotionManager>();
+            enemyAnimatorManager = GetComponentInChildren<EnemyAnimatorManager>();
         }
 
         private void Update() 
         {
-
+            HandleRecoveryTimer();
         }
 
         private void FixedUpdate() 
@@ -31,14 +37,108 @@ namespace SG
 
         private void HandleCurrentAction()
         {
+            if (enemyLocomotionManager.currentTarget != null)
+            {
+                enemyLocomotionManager.distanceFromTarget = Vector3.Distance(enemyLocomotionManager.currentTarget.transform.position, transform.position);
+            }
+
             if (enemyLocomotionManager.currentTarget == null)
             {
                 enemyLocomotionManager.HandleDerection();
             }
-            else
+            else if (enemyLocomotionManager.distanceFromTarget > enemyLocomotionManager.stoppingDistance)
             {
                 enemyLocomotionManager.HandleMoveToTarget();
             }
+            else if(enemyLocomotionManager.distanceFromTarget <= enemyLocomotionManager.stoppingDistance)
+            {
+                AttackTarget();
+            }
         }
+
+        private void HandleRecoveryTimer()
+        {
+            if(currentRecoveryTime > 0)
+            {                
+                currentRecoveryTime -= Time.deltaTime;
+            }
+
+            if(isPreformingAction)
+            {
+               if (currentRecoveryTime <= 0)
+               {
+                   isPreformingAction = false;
+               }
+            }
+        }
+        #region Attacks
+
+        private void AttackTarget()
+        {
+            if(isPreformingAction)
+            return;
+
+            if (currentAttack == null)
+            {
+                GetNewAttack();
+            }
+            else
+            {
+                isPreformingAction = true;
+                currentRecoveryTime = currentAttack.recoveryTime;
+                enemyAnimatorManager.PlayTargetAnimation(currentAttack.actionAnimation, true);
+                currentAttack = null;
+            }
+        }
+
+        private void GetNewAttack()
+        {
+            Vector3 tagretDirection = enemyLocomotionManager.currentTarget.transform.position - transform.position;
+            float viewableAngle = Vector3.Angle(tagretDirection, transform.forward);
+            enemyLocomotionManager.distanceFromTarget = Vector3.Distance(enemyLocomotionManager.currentTarget.transform.position, transform.position);
+
+            int maxScore = 0;
+
+            for (int i = 0; i < enemyAttacks.Length; i++)
+            {
+                EnemyAttackAction enemyAttackAction = enemyAttacks[i];
+
+                if (enemyLocomotionManager.distanceFromTarget <= enemyAttackAction.maximumDistanceNeededToAttack && enemyLocomotionManager.distanceFromTarget >= enemyAttackAction.mininumDistanceNeededToAttack)
+                {
+                    if (viewableAngle <= enemyAttackAction.maximumAttackAngle 
+                    && viewableAngle >= enemyAttackAction.minimumAttackAngle)
+                    {
+                        maxScore += enemyAttackAction.attackScore;
+                    }
+                }
+            }
+            
+            int randomValue = Random.Range(0, maxScore);
+            int temporaryScore = 0;
+
+            for (int i = 0; i < enemyAttacks.Length; i++)
+            {
+                EnemyAttackAction enemyAttackAction = enemyAttacks[i];
+
+                if (enemyLocomotionManager.distanceFromTarget <= enemyAttackAction.maximumDistanceNeededToAttack
+                && enemyLocomotionManager.distanceFromTarget >= enemyAttackAction.mininumDistanceNeededToAttack)
+                {
+                    if (viewableAngle <= enemyAttackAction.maximumAttackAngle 
+                    && viewableAngle >= enemyAttackAction.minimumAttackAngle)
+                    {
+                       if (currentAttack != null)
+                       return;
+
+                       temporaryScore += enemyAttackAction.attackScore;
+
+                       if (temporaryScore > randomValue)
+                       {
+                           currentAttack = enemyAttackAction;
+                       }
+                    }
+                }
+            }
+        }
+        #endregion
     }
 }
